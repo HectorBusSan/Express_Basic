9dfa14666d642699160397c653c66ed82b38a75a
Hector <hector141599@gmail.com>
2022-05-04 08:36:31 -0500

    Paso 4: Agregando nuevo router /LaunchX

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app= express();
 // vamos a declarar el puerto localhost:3000
 const port = 3000
+
 // url
 // vamos poner diagonal para por default localhost
 // te pide un request and response
@@ -10,6 +11,11 @@ app.get('/',(req,res)=>{
     res.send('Vamos con Tokio Héctor!!!!!!!!!');
 });
 // para ver los nuevos resultaods paras el server y lo vuelves a correr
+
+// ahorale agregamos a la diagonal
+app.get('/launchX',(req,res)=>{
+    res.send("Bienvenidos a LaunchX")
+})
 
 // inicializar
 app.listen(port,()=>{
