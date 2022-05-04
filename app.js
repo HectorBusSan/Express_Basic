f863c64f8b3c83fc970a301bb450fbf716b67407
Hector <hector141599@gmail.com>
2022-05-04 08:51:35 -0500

    Paso 6: Agregando nuevo route /explorers/:explorer y recibiendo query params

--- app.js
+++ app.js
@@ -17,12 +17,24 @@ app.get('/launchX',(req,res)=>{
     res.send("Bienvenidos a LaunchX")
 })
 
-// interesante
+// interesante: regresa objetos
 app.get('/node',(req,res)=>{
     const explorer1= {id:1,name:"Explorer1"};
     const explorer2= {id:2,name:"Explorer2"};
     const explorer3= {id:3,name:"Explorer3"};
     res.send([explorer1,explorer2,explorer3]);
+})
+
+// webpads
+// la url me envia un parametro
+// se guarda en el request
+// y se guarda en params
+// ya nos regresa un objeto con el nombre del explorer
+// http://localhost:3000/explorers/Hector
+app.get('/explorers/:explorer',(req,res)=>{
+    console.log(req.params);
+    res.send(req.params);
+    // {"explorer":"Hector"}
 })
 
 // inicializar
